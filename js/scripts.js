6605ae2367eed849e3e6363ff6a42c05cf81f1a1
Katrina Hockman and Robert Bruce <robert@>
2018-10-22 16:13:31 -0700

    fix sorting algorithm and return array with descending freq of words

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,10 @@ function countWord(list, word) {
 
   return count;
 };
-
-
+var sortedArray = [];
 var uniqueArray = [];
 var countArray = [];
+var tempArray = [];
 
 function checkWord(list) {
 
@@ -29,21 +29,50 @@ function checkWord(list) {
 };
 
 
+function combineAndSort(wordArray, numberArray) {
+  for (var i = wordArray.length; i > 0; i--) {
+    console.log(numberArray);
+    var max = Math.max(...numberArray);
+    console.log("max = ", max);
+    var index = numberArray.indexOf(max);
+    console.log("index = ", index);
+    tempArray = [];
+    tempArray.push(wordArray[index]);
+    tempArray.push(numberArray[index]);
+    console.log(tempArray);
+    sortedArray.push(tempArray);
+
+    if (wordArray.length > 2) {
+      wordArray.splice(index, 1);
+      numberArray.splice(index, 1);
+    } else {
+      debugger;
+      wordArray.pop();
+      numberArray.pop();
+    }
+
+  }
+  uniqueArray.forEach(function(each) {
+    sortedArray.push([each, 1]);
+  })
+}
 
 
 $(function() {
 
   $("#form").submit(function(event) {
     event.preventDefault();
+    sortedArray = [];
 
     var inputString = $("#bulk-input").val();
     var inputArray = inputString.split(" ");
 
     checkWord(inputArray);
-
+    combineAndSort(uniqueArray, countArray)
 
     console.log(uniqueArray);
     console.log(countArray);
+    console.log(sortedArray)
 
   });
 
