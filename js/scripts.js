100f24a7e0878a4e9d5c6f5a50e987485b52b4fc
Katrina Hockman and Robert Bruce <katrina@>
2018-10-22 14:20:16 -0700

    Add to js functions; unsure what we did; still confused but it works

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,50 @@
+function countWord(list, word) {
+  var count = 0;
+  list.forEach(function(each) {
+    if (word === each) {
+      count += 1;
+    }
+  });
+
+  return count;
+};
+
+
+var uniqueArray = [];
+var countArray = [];
+
+function checkWord(list) {
+
+  for (var i = 0; i < list.length; i++) {
+    var number = countWord(list, list[i]);
+
+    if (uniqueArray.indexOf(list[i]) != -1) {
+        // pass
+      } else {
+        uniqueArray.push(list[i]);
+        console.log("uniqueArray= ", uniqueArray);
+        countArray.push(number);
+        console.log("countArray = " + countArray);
+      }
+  }
+  return true;
+};
+
 $(function() {
 
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+    var inputString = $("#bulk-input").val();
+    var inputArray = inputString.split(" ");
+
+    checkWord(inputArray);
+
+
+    console.log(uniqueArray);
+    console.log(countArray);
+
+  });
 
 
 });
